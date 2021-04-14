8c9d4fca8edafd90f31a37b178e807c887018b40
Ovi Savescu <ovi@ovswebsites.com>
2021-04-14 09:59:57 +0300

    updated regex for detecting external link

--- components/ButtonC.vue
+++ components/ButtonC.vue
@@ -1,5 +1,5 @@
 <template>
-  <component v-bind="linkProps(url)" class="button">
+  <component v-bind="linkProps(url)" class="button" :class="buttonClasses">
     <slot></slot>
     <svg-icon
       name="icon-chevron-right-light"
@@ -16,11 +16,27 @@ export default {
     url: {
       type: String,
       required: true
+    },
+    bgColor: {
+      type: String,
+      default: "green"
+    },
+    color: {
+      type: String,
+      default: "dark"
+    }
+  },
+  computed: {
+    buttonClasses: function() {
+      // `this` points to the vm instance
+      return `bg-${this.bgColor} text-${this.color}`;
     }
   },
   methods: {
     linkProps(url) {
-      if (url.match(/^(http(s)?|ftp):\/\//)) {
+      if (
+        url.match(/((mailto:\w+)|(tel:\w+)|(http:\/\/\w+)|(https:\/\/\w+)).+/)
+      ) {
         return {
           is: "a",
           href: url,
