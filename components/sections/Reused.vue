ad1b9a0a0210c0d70c9693d601a0f8deb8f6850a
Ovi Savescu <ovi@ovswebsites.com>
2021-04-20 09:07:45 +0300

    reused section fetch data from sanity

--- components/sections/Reused.vue
+++ components/sections/Reused.vue
@@ -1,20 +1,47 @@
 <template>
   <div>
-    reused section
+    <SectionsRenderer :sections="reusedSectionContents.sections" />
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      section: {
-        type: Object,
-        required: true
-      },
-    },
+const query = /* groq */ `*[_type == 'reusableSection' && _id == $reusableSectionId][0]`;
+
+export default {
+  props: {
+    section: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      reusedSectionContents: {}
+    };
+  },
+
+  activated() {
+    this.$fetch();
+  },
+
+  async fetch() {
+    // console.log("propsss", this._props.section.reusableSection._ref);
+    const reusedSectionId = this._props.section.reusableSection._ref;
+    this.reusedSectionContents = await this.$nuxt.context.$sanity.fetch(query, {
+      reusableSectionId: reusedSectionId
+    });
   }
+
+  // asyncData({ $sanity, params, payload }) {
+  //   if (payload) {
+  //     return { page: payload };
+  //   }
+  //   return $sanity.fetch(query, {
+  //     slug: params.page
+  //   });
+  // }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
