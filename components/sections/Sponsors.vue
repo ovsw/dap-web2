26a7ebc57f95bcf8d2ed1ea4b461e038810c47bd
Ovi Savescu <ovi@ovswebsites.com>
2021-09-22 18:34:34 +0300

    fixes #1: sponsor logos squished on mobile.

--- components/sections/Sponsors.vue
+++ components/sections/Sponsors.vue
@@ -8,21 +8,21 @@
               <span v-if="section.title">{{ section.title }}</span
               ><span v-else>Sponsors</span>
             </h2>
-            <div class="[ sponsorsSection__content ] [ pannel flex flex-wrap ]">
+            <div class="[ sponsorsSection__content ] [ pannel md:flex flex-wrap ]">
               <a
                 v-for="(sponsor, index) in section.sponsorsList"
                 :key="index"
                 :href="sponsor.url"
                 target="_blank"
                 rel="noopener"
-                class="sponsorGridCell w-1/4 flex items-center justify-center px-6"
+                class="sponsorGridCell md:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 flex justify-center"
               >
                 <!-- <img
                   :src="$urlFor(sponsor.image).width(200)"
                   :alt="`${sponsor.name} logo`"
                 /> -->
-                <img
-                  :src="$urlFor(sponsor.image).height(100)"
+                <img class="sponsorGridCell  flex items-center justify-center px-10 py-6   object-contain"
+                  :src="$urlFor(sponsor.image).height(200)"
                   :alt="`${sponsor.name} logo`"
                 />
               </a>
