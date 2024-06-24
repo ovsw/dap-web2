9706ef82fc49d9ce35808e5973c73359e3aa6a69
OVIS <ovi@ovswebsites.com>
2024-06-24 08:51:59 +0300

    added console.log to rides page title

--- pages/amusement-park-rides/_page.vue
+++ pages/amusement-park-rides/_page.vue
@@ -37,6 +37,7 @@ export default {
 
   computed: {
     seoTitle() {
+      console.log("page", this.page);
       if (this.page.content.seo && this.page.content.seo.title)
         return this.page.content.seo.title;
       return this.page.title;
